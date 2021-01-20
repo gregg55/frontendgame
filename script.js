d7e5eb81a3fa52437239991b7b4c3e2f4fddcb65
Machetta Gregg R <machettagreggr@johndeere.com>
2021-01-20 11:17:51 -0600

    adding photo/rule and footer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log ("hello World"); // validated js script worked in Console log
 
-let name = prompt("Type Good Luck");
-alert("welcome to Hang Man");
+// let name = prompt("Type Good Luck");
+// alert("welcome to Hang Man");
