cfbb7a98bd00b8a85d93e245ad83942499711af6
Machetta Gregg R <machettagreggr@johndeere.com>
2021-01-21 09:52:44 -0600

    console log js as a TEST

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 
 console.log("hello");
-
-let keyValue =[];
-let board = document.querySelectorAll(".letter") // pull keyinputs into the variable board
-console.log (board);
-
-board.addEventListener("click", function(){
-    for (let i=0; i < board.length; i++)  {
-        let keyValue = board[i].value;
-        console.log(board[i].value)
-}
-});
