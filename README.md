9b703edf23b62c5382f6a705b3ad027a560895ec
Machetta Gregg R <machettagreggr@johndeere.com>
2021-01-22 17:20:44 -0600

    start JS

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # frontendgame
 
-This is a Sports Trivia Game.
+Description:
+
+This is a Sports Trivia Game. 
+It's a trivia game about some of the greatest sports athletes of all time.
+
+Brief Example:
 
 You will need to research in 3 areas prior to taking this test.
 The areas that you need to search is Basketball, Baseball, and Football.
 
+List of Features
+
+Select only 1 answer per question. Think before you select the correct answer
 You will be asked questions based on your Sports research.
 
-You will need to select only 1 answer from each question.
+Getting Started
 
+Click the Start Button
+You will need to select only 1 answer from each question.
 At the end of the 10 questions, you will be provided a score.
 
 Additionally, you will be evaluated on your comprehension for sports research.
+If you would like to retake the test. Then just press the Retake button
 
 Good luck!
--- script.js
+++ script.js
@@ -1,1 +1,115 @@
+console.log("Sports Trivia Game is Fun!");
 
+
+
+
+let start= document.querySelector('.start')
+let option = document.querySelectorAll('.option')
+
+let directions= document.querySelector('.directions')
+
+
+
+
+// <------------------   View All Questions in Class .q ---------->
+// view & console log all values of Class .q  -  shows all questions
+
+let q = document.querySelectorAll('.q')
+console.log(q) // Worked Correctly
+
+// <------------------   Restart Section ---------->
+// Set up START on Click - Use query selector & Event Listener
+
+let currentQIndex = 0
+let previousQIndex = 0
+let score = 0
+
+start.addEventListener('click', startTrivia)
+function startTrivia(){
+q[currentQIndex].style.display = 'block' 
+//this will make the first question load
+console.log('current', q[currentQIndex])
+directions.style.display= 'none'
+//removes the directions and start button 
+}
+console.log(q)
+// it makes work buttons work
+option.forEach(buttons =>{
+buttons.addEventListener('click', next)
+})
+
+function next(evt){
+console.log('this is next')
+evt.preventDefault()
+//this prevents the page from default 
+previousQIndex = currentQIndex
+currentQIndex++
+console.log(q[currentQIndex])
+q[previousQIndex].style.display = 'none' 
+//removes last question
+q[currentQIndex].style.display = 'block' 
+//display next question
+}
+
+
+// <------------------   Scoring Section  ---------->
+// Set up Score Criteria & Grading  - Use IF /ELSE IF Statements
+
+let result = document.querySelector('.result') // pull result data 
+
+
+
+function updateScore(){
+score++ // increment operator (++) adds one to operand and returns value
+if(score === 10){
+result.textContent = '100% - You are a Sports Fan!'
+} else if(score === 9 ){
+result.textContent= '90% Great job - Go Hawks!'
+} else if( score === 8){
+result.textContent = '80% Good job- Go Hawks!!'
+} else if( score === 7){
+result.textContent= '70% Good job- Go Hawks!!'
+}else if(score === 6){
+result.textContent = '60% Looks Good - Go Hawks!!'
+}else if(score === 5){
+result.textContent = '50% Looks Good - Go Hawks!'
+}else if(score === 4){
+result.textContent = '40% Looks Good - Go Hawks!'
+}else if(score === 3){
+result.textContent = '30% Looks Good - Go Hawks!'
+}else if(score === 2){
+result.textContent= '20% Looks Good - Go Hawks!'
+}else if(score === 1){
+result.textContent = '10% Looks Good - Go Hawks!'
+}else if(score === 0){
+result.textContent = '0% Looks Good - Go Hawks!'
+}
+}
+console.log('updated score', score) // Console log - score has been updated
+
+// <------------------   Correct Answer ---------->
+// Find Correct answers & pull this data
+let correct =document.querySelectorAll('.correct') // pull class.correct and place into variable correct
+
+for(let i=0; i<correct.length; i++){  // loop through correct.length
+correct[i].addEventListener('click', correctAnswer) // on event click - place correct answers into correctAnswer
+console.log('looping for correct answers')  // console log 
+}
+
+
+function correctAnswer(evt){
+console.log('evt target', evt.target) //
+if(evt.target.classList.contains('correct')){  // the event.target.className returns an array of correct answers
+console.log('Placing correct anwers ')
+updateScore()  // update Score based on if / Else 
+}
+}
+
+// <------------------   Restart Section ---------->
+// Set up Restart on Click - Use query selector & Event Listener
+let restart = document.querySelector('.restart')
+restart.addEventListener('click', restartTrivia)
+function restartTrivia(){
+//window.location.reload(true); // Restart - this works also - as an option for reload - I went with STACK Overflow
+window.location.href = window.location.href; // to Restart - per Slack Overflow
+}
